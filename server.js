1ffecb28a32471f26ef0845fa204265d6bb793ef
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-10-09 15:02:25 -0500

    Added mutations to add book and author

--- server.js
+++ server.js
@@ -91,8 +91,41 @@ const RootQueryType = new GraphQLObjectType({
   }),
 });
 
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString)},
+                authorId: { type: GraphQLNonNull(GraphQLInt)}
+            },
+            resolve: (parent, args) => {
+                const book = {id: books.length + 1, name: args.name, authorId: args.authorId}
+                books.push(book)
+                return book
+            } 
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an Author',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString)}
+            },
+            resolve: (parent, args) => {
+                const author = {id: authors.length + 1, name: args.name}
+                authors.push(author)
+                return author
+            } 
+        }
+    })
+})
+
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  mutation: RootMutationType,
 });
 
 app.use(
