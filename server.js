0b60ef22068762eb39222b0cc280f6effe9c1c1b
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-10-09 14:12:46 -0500

    Hello world graphql

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+} = require('graphql')
+const app = express();
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+})
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
+
+
+app.listen(5000., () => console.log('Server Running'));
